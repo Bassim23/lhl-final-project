729546a3ea69cddc1fe2a3099172b68bae4b7844
Ben Li <ben_li5@yahoo.ca>
2017-04-27 19:27:01 +0000

    changes to append

--- server/app/assets/javascripts/search_trips.js
+++ server/app/assets/javascripts/search_trips.js
@@ -21,24 +21,22 @@ jQuery(document).ready(function() {
 function createTripsElement(trips) {
   let $trip =
     `
-      <div class="span4" id="item-eilu-2017">
-        <div class="triptextcontent  clearfix">
-          <div class="card">
+      <div class="col-md-4">
+        <div class="card">
+          <div class="card-block">
+            <div class="view overlay hm-white-slight">
+              <img src="https://media.gadventures.com/media-server/cache/a5/7d/a57d03f90d0e6743fe4e4aadc3e8bf55.jpg"img-fluid" alt="" />
+              <a href={"#"}>
+                <div class="mask waves-effect waves-light">
+                </div>
+              </a>
+            </div>
             <div class="card-block">
-              <div class="view overlay hm-white-slight">
-                <img src="https://media.gadventures.com/media-server/cache/a5/7d/a57d03f90d0e6743fe4e4aadc3e8bf55.jpg"img-fluid" alt="" />
-                <a href={"#"}>
-                  <div class="mask waves-effect waves-light">
-                  </div>
-                </a>
-              </div>
-              <div class="card-block">
-                <h4 class="card-title">${trips.destination}</h4>
-                <div class="card-event">${trips.trip.name}</div>
-                <div class="card-host"> Host: ${trips.trip.user.first_name} ${trips.trip.user.last_name} </div>
-                <div class="card-date">${trips.date}</div>
-                <a href={"#"} class="btn btn-small">View this Trip</a>
-              </div>
+              <h4 class="card-title">${trips.destination}</h4>
+              <div class="card-event">${trips.trip.name}</div>
+              <div class="card-host"> Host: ${trips.trip.user.first_name} ${trips.trip.user.last_name} </div>
+              <div class="card-date">${trips.date}</div>
+              <a href={"#"} class="btn btn-small">View this Trip</a>
             </div>
           </div>
         </div>
