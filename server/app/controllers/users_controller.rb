ec06fdbda286514718d72ad409d16632b6a5a82c
Vinay Balaji <vinaybalaji@email.com>
2017-04-29 17:13:36 +0000

    Added models controllers and views for user login and register features

---
+++ server/app/controllers/users_controller.rb
@@ -0,0 +1,67 @@
+class UsersController < ApplicationController
+  
+  def new
+  end
+
+  def create
+    new_user = create_user
+    if new_user
+      session[:user_id] = new_user.id
+      redirect_to root_path
+    else
+      redirect_to :back
+    end
+  end
+
+  private
+
+  def create_user
+    user = params[:user]
+    first_name = user[:first_name]
+    last_name = user[:last_name]
+    age = user[:age]
+    gender = user[:gender]
+    nationality = user[:nationality]
+    email = user[:email]
+    summary = user[:summary]
+    password = user[:password]
+    password_confirmation = user[:password_confirmation]
+    if first_name.blank? || last_name.blank?
+      message = "Oops! Please enter a first name and last name."
+    elsif age.blank?
+      message = "Oops! Please enter your age."
+    elsif gender.blank?
+      message = "Oops! Please enter your gender."
+    elsif nationality.blank?
+      message = "Oops! Please enter your nationality."
+    elsif email.blank?
+      message = "Oops! Please enter a valid email."
+    elsif password.blank? || password_confirmation.blank?
+      message = "Oops! Please enter a valid password."
+    elsif password != password_confirmation
+      message = "Oops! Passwords don't match. Please try again."
+    elsif User.exists?(email: email)
+      message = "Oops! E-mail already exists. Please try again or log in with your existing email."
+    end
+    if message
+      flash[:notice] = message
+      return
+    end
+    new_user = User.new(
+      first_name: first_name,
+      last_name: last_name,
+      age: age,
+      gender: gender,
+      nationality: nationality,
+      email: email,
+      summary: summary,
+      password: password,
+      password_confirmation: password_confirmation,
+      upvotes: 0,
+      downvotes: 0
+    )
+    new_user.save!
+    new_user
+  end
+  
+end
--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -1,6 +1,47 @@
-class User < ApplicationRecord
+class User < ActiveRecord::Base
+  has_secure_password
   has_many :trips, dependent: :destroy
   has_many :participations, dependent: :destroy
   has_many :activities, through: :participations, dependent: :destroy
+
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :age, presence: true
+  validates :gender, presence: true
+  validates :nationality, presence: true
+  validate :passwords_should_match
+  validate :password_length
+  validate :email_exists
+
+ private
+
+  def passwords_should_match
+    if password != password_confirmation
+      errors.add(:password, "Passwords don't match!")
+    end
+  end
+
+  def email_exists
+    if User.exists?(email: email)
+      errors.add(:email, "Email already exists!")
+    end
+  end
+
+  def password_length
+    if !password.nil? && password.length < 5
+      errors.add(:password, "Password is not at least 5 characters long")
+    end
+  end
+
+  def self.authenticate_with_credentials (email, password)
+    email = email.downcase.strip
+    user = User.find_by_email(email)
+    if user && user.authenticate(password)
+      return user
+    end
+  end
 
 end
