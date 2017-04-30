36a6bc4f58005a9b3d2f513549698b8ca896859c
Vinay Balaji <vinaybalaji@email.com>
2017-04-30 01:20:50 +0000

    Added current user helper method and before filter for trips controller

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -11,4 +11,11 @@ class ApplicationController < ActionController::Base
     redirect_to login_path unless current_user
   end
 
+  private
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+
 end
