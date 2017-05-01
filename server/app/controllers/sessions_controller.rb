4580cff92e0a2dda53fbe40632b4f849bd99eaeb
Vinay Balaji <vinaybalaji@email.com>
2017-05-01 18:34:36 +0000

    In progress redirection of login failure to user login model

--- server/app/controllers/sessions_controller.rb
+++ server/app/controllers/sessions_controller.rb
@@ -6,10 +6,10 @@ class SessionsController < ApplicationController
   def create
     if user = User.authenticate_with_credentials(params[:session][:email], params[:session][:password])
       session[:user_id] = user.id
-      redirect_to root_path
+      redirect_to :back
     else
       flash[:notice] = "Oops, email or password is invalid. Please try again."
-      redirect_to login_path
+      render partial: "sessions/new", :class => "login-modal-toggle", :remote => true, "data-target" => "#login-modal"
     end
   end
 
